8594bda04778c9c169e23ab5f5543345a719e1fc
quavious <leenowon97@gmail.com>
2020-02-28 19:56:51 +0900

    Updates

---
+++ precache-manifest.6701ab7049514ad9322a702bcbc45ca2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02528fad5ad84a0cd6766995061c4e6b",
+    "url": "/new_pott/index.html"
+  },
+  {
+    "revision": "14a423b74a455b9a138e",
+    "url": "/new_pott/static/css/main.b7179ac9.chunk.css"
+  },
+  {
+    "revision": "d10add6933d7ac957e04",
+    "url": "/new_pott/static/js/2.65418bf6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/new_pott/static/js/2.65418bf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a423b74a455b9a138e",
+    "url": "/new_pott/static/js/main.8f29cec8.chunk.js"
+  },
+  {
+    "revision": "bf661c856ec12764b280",
+    "url": "/new_pott/static/js/runtime-main.0a042cbe.js"
+  },
+  {
+    "revision": "6d20d3198b9b3658b652bac198623139",
+    "url": "/new_pott/static/media/Castle.6d20d319.jpg"
+  },
+  {
+    "revision": "8ac9b07bb9aafda25ec855b6dcd904d4",
+    "url": "/new_pott/static/media/GreatWall.8ac9b07b.jpg"
+  }
+]);
